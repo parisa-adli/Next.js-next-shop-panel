d653c8701529d1b9ab8deef1d986a2c48789d284
parisa-adli <parisa.adli1996@gmail.com>
2025-03-18 16:57:31 +0330

    sendOTP component

--- src/app/auth/SendOTPForm.jsx
+++ src/app/auth/SendOTPForm.jsx
@@ -1,15 +1,16 @@
 import TextField from "@/common/TextField";
 
-function SendOTPForm({ phoneNumber, onChange }) {
+function SendOTPForm({ phoneNumber, onChange, onSubmit }) {
   return (
     <div>
-      <form>
+      <form className="space-y-10" onSubmit={onSubmit}>
         <TextField
           label="شماره موبایل"
           name="phoneNumber"
           value={phoneNumber}
           onChange={onChange}
         />
+        <button className="btn btn--primary w-full">ارسال کد تایید</button>
       </form>
     </div>
   );
