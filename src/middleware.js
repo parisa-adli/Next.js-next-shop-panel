8229be4b0145c2251c45a86cfb8db9471acb6679
parisa-adli <parisa.adli1996@gmail.com>
2025-03-26 12:42:37 +0330

    refactor middlewareAuth function

--- src/middleware.js
+++ src/middleware.js
@@ -1,36 +1,22 @@
 import { NextResponse } from "next/server";
+import middlewareAuth from "./utils/middlewareAuth";
 
 export async function middleware(req) {
-  const url = req.url; // -> http://localhost:3000/admin
-  const pathname = req.nextUrl.pathname; // -> /admin
-  //   console.log(url, pathname);
+  const url = req.url;
+  const pathname = req.nextUrl.pathname;
   if (pathname.startsWith("/profile")) {
-    // console.log(req.cookies.get("accessToken")); -> {name,value}
-    let strCookie = "";
-    req.cookies.getAll().forEach((item) => {
-      strCookie += `${item?.name}=${item.value}; `;
-    });
-
-    const { data } = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/user/profile`,
-      {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Cookie: strCookie,
-        },
-      }
-    ).then((res) => res.json());
-    const { user } = data || {};
-      if (!user) NextResponse.redirect(new URL("/auth", url));
-      console.log(user);
+    const user = await middlewareAuth(req);
+    if (!user) return NextResponse.redirect(new URL("/auth", url));
   }
 
   if (pathname.startsWith("/admin")) {
-    console.log("this is admin req!");
+    const user = await middlewareAuth(req);
+    if (!user) return NextResponse.redirect(new URL("/auth", url));
+    if (user && user.role !== "ADMIN")
+      return NextResponse.redirect(new URL("/", url));
   }
 }
 
 export const config = {
-  matcher: ["/admin", "/profile"],
+  matcher: ["/admin/:path*", "/profile/:path*"],
 };
