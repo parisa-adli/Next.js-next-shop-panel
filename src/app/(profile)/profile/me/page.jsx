8e512475e3c077b9c5fb1e6954d79d87fa2e3481
parisa-adli <parisa.adli1996@gmail.com>
2025-03-26 21:20:16 +0330

    update user profile

--- src/app/(profile)/profile/me/page.jsx
+++ src/app/(profile)/profile/me/page.jsx
@@ -1,27 +1,46 @@
 "use client";
 
+import Button from "@/common/Button";
 import Loading from "@/common/Loading";
 import TextField from "@/common/TextField";
 import { useGetUser } from "@/hooks/useAuth";
+import { updateProfile } from "@/services/authServices";
 import { includeObj } from "@/utils/objectUtils";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 function MePage() {
   const { isLoading, data } = useGetUser();
+  const queryClient = useQueryClient();
   const [formData, setFormData] = useState({});
   const { user } = data || {};
   const includeskey = ["name", "email", "phoneNumber", "biography"];
+  const { isPending: isUpdating, mutateAsync } = useMutation({
+    mutationFn: updateProfile,
+  });
 
   useEffect(() => {
     if (user) setFormData(includeObj(user, includeskey));
   }, [user]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { message } = await mutateAsync(formData);
+      queryClient.invalidateQueries({ queryKey: ["get-user"] });
+      toast.success(message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
 
   if (isLoading) return <Loading />;
 
   return (
     <div className="max-w-sm">
       <h1 className="text-xl font-bold mb-4">اطلاعات کاربری</h1>
-      <form action="" className="space-y-8">
+      <form onSubmit={submitHandler} className="space-y-8">
         {Object.keys(includeObj(user, includeskey)).map((key) => {
           return (
             <TextField
@@ -35,6 +54,13 @@ function MePage() {
             />
           );
         })}
+        {isUpdating ? (
+          <div className="flex justify-center">
+            <Loading width="45" height="15" />
+          </div>
+        ) : (
+          <Button>ارسال کد تایید</Button>
+        )}
       </form>
     </div>
   );
