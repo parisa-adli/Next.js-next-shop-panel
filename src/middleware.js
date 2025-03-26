818b4763156452ab531d643de8f643ff96dd83f1
parisa-adli <parisa.adli1996@gmail.com>
2025-03-26 12:11:58 +0330

    fetch user in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,34 @@
-export function middleware(req) {
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
   const url = req.url; // -> http://localhost:3000/admin
   const pathname = req.nextUrl.pathname; // -> /admin
-    //   console.log(url, pathname);
-    if (pathname.startsWith("/admin")) {
-        console.log("this is admin req!");
-    }
+  //   console.log(url, pathname);
+  if (pathname.startsWith("/profile")) {
+    // console.log(req.cookies.get("accessToken")); -> {name,value}
+    let strCookie = "";
+    req.cookies.getAll().forEach((item) => {
+      strCookie += `${item?.name}=${item.value}; `;
+    });
 
-    if (pathname.startsWith("/profile")) {
-      console.log("this is profile req!");
-    }
+    const { data } = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/user/profile`,
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Cookie: strCookie,
+        },
+      }
+    ).then((res) => res.json());
+    const { user } = data || {};
+      if (!user) NextResponse.redirect(new URL("/auth", url));
+      console.log(user);
+  }
+
+  if (pathname.startsWith("/admin")) {
+    console.log("this is admin req!");
+  }
 }
 
 export const config = {
