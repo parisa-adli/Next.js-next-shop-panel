874d833901ffa3503d6f1264999a41b2ec39cc6c
parisa-adli <parisa.adli1996@gmail.com>
2025-03-28 15:00:51 +0330

    sort products

---
+++ src/app/(user)/products/ProductsFilter.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import CheckBox from "@/common/CheckBox";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useState } from "react";
+
+function ProductsFilter({ categories }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [selectedCategories, setSelectedCategories] = useState(
+    searchParams.get("category")?.split(",") || []
+  );
+  // console.log(searchParams.get("category")?.split(",") || []);
+
+  const createQueryString = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  const categoryHandler = (e) => {
+    const value = e.target.value;
+    if (selectedCategories.includes(value)) {
+      const categories = selectedCategories.filter((c) => c !== value);
+      setSelectedCategories(categories);
+      router.push(pathname + "?" + createQueryString("category", categories));
+    } else {
+      setSelectedCategories([...selectedCategories, value]);
+      router.push(
+        pathname +
+          "?" +
+          createQueryString("category", [...selectedCategories, value])
+      );
+    }
+  };
+  return (
+    <div>
+      <p className="font-bold mb-4">دسته بندی ها</p>
+      <ul className="space-y-4">
+        {categories.map((category) => (
+          <CheckBox
+            key={category._id}
+            id={category._id}
+            label={category.title}
+            name="product-type"
+            value={category.englishTitle}
+            onChange={categoryHandler}
+            checked={selectedCategories.includes(category.englishTitle)}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default ProductsFilter;
