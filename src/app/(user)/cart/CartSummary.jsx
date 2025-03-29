f75a3abfef2f77cd4aa159e89ebc25a2e8b1d48f
parisa-adli <parisa.adli1996@gmail.com>
2025-03-29 14:55:16 +0330

    CartSummary component

---
+++ src/app/(user)/cart/CartSummary.jsx
@@ -0,0 +1,29 @@
+import Button from "@/common/Button";
+
+function CartSummary({ payDetail }) {
+  const { totalOffAmount, totalPrice, totalGrossPrice } = payDetail;
+
+  return (
+    <div className="border p-2 pb-4 rounded-xl space-y-8">
+      <p className="font-bold p-2 text-secondary-700 text-center border-b">
+        اطلاعات پرداخت
+      </p>
+      <div className="space-y-4 pb-4 border-b border-dashed">
+        <div className="flex items-center justify-between">
+          <span>جمع کل</span>
+          <span>{totalGrossPrice}</span>
+        </div>
+        <div className="flex items-center justify-between">
+          <span>تخفیف</span>
+          <span>{totalOffAmount} -</span>
+        </div>
+        <div className="flex items-center justify-between font-bold">
+          <span>مبلغ قابل پرداخت</span>
+          <span>{totalPrice}</span>
+        </div>
+      </div>
+      <Button>ثبت سفارش</Button>
+    </div>
+  );
+}
+export default CartSummary;
--- src/app/(user)/cart/page.jsx
+++ src/app/(user)/cart/page.jsx
@@ -4,6 +4,7 @@ import Loading from "@/common/Loading";
 import { useGetUser } from "@/hooks/useAuth";
 import Link from "next/link";
 import CartItem from "./CartItem";
+import CartSummary from "./CartSummary";
 
 function CartPage() {
   const { data, isLoading } = useGetUser();
@@ -39,7 +40,7 @@ function CartPage() {
             <CartItem key={item._id} cartItem={item} />
           ))}
       </div>
-      <div className="col-span-1">cart summary</div>
+      <div className="col-span-1"><CartSummary payDetail={cart.payDetail} /></div>
     </div>
   );
 }
