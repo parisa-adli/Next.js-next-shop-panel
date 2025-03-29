4102fe46c1aa3889c77b8d7697569bea7d78fa03
parisa-adli <parisa.adli1996@gmail.com>
2025-03-29 12:08:15 +0330

    Breadcrumb

--- src/app/(user)/products/[slug]/page.jsx
+++ src/app/(user)/products/[slug]/page.jsx
@@ -1,3 +1,4 @@
+import BreadCrumbs from "@/common/BreadCrumbs";
 import { getOneProductBySlug, getProducts } from "@/services/productService";
 
 export const dynamic = "force-static"; // SSG or {cache: "force-cache"}
@@ -15,30 +16,49 @@ async function SlugPage({ params }) {
   const { product } = await getOneProductBySlug(slug);
 
   return (
-    <div>
-      <h1 className="font-bold text-2xl mb-6">{product.title}</h1>
-      <p className="mb-6">{product.description}</p>
-      <p className="mb-6">
-        <span>قیمت محصول: </span>
-        <span className={`${product.discount ? "line-through" : "font-bold"}`}>
-          {product.price}
-        </span>
-      </p>
-      {!!product.discount && (
-        <div className="flex items-center gap-x-2 mb-6">
-          <p className="text-xl font-bold">قیمت با تخفیف: {product.offPrice}</p>
-          <div
-            className="
-                  bg-rose-500 px-2 py-0.5 rounded-xl text-white text-sm"
-          >
-            {product.discount} %
-          </div>
-        </div>
-      )}
+    <>
+      <BreadCrumbs
+        breadcrumbs={[
+          {
+            label: "محصولات",
+            href: "/products",
+          },
+          {
+            label: `${product.title}`,
+            href: `/products/${product.slug}`,
+            active: true,
+          },
+        ]}
+      />
       <div>
-        <button className="btn btn--primary">اضافه کردن به سبد خرید</button>
+        <h1 className="font-bold text-2xl mb-6">{product.title}</h1>
+        <p className="mb-6">{product.description}</p>
+        <p className="mb-6">
+          <span>قیمت محصول: </span>
+          <span
+            className={`${product.discount ? "line-through" : "font-bold"}`}
+          >
+            {product.price}
+          </span>
+        </p>
+        {!!product.discount && (
+          <div className="flex items-center gap-x-2 mb-6">
+            <p className="text-xl font-bold">
+              قیمت با تخفیف: {product.offPrice}
+            </p>
+            <div
+              className="
+                  bg-rose-500 px-2 py-0.5 rounded-xl text-white text-sm"
+            >
+              {product.discount} %
+            </div>
+          </div>
+        )}
+        <div>
+          <button className="btn btn--primary">اضافه کردن به سبد خرید</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 export default SlugPage;
