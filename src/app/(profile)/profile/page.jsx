51e54c296e800a7eec4ae82cd4987b4b89388eb8
parisa-adli <parisa.adli1996@gmail.com>
2025-03-30 13:27:45 +0330

    create dashboard page

--- src/app/(profile)/profile/page.jsx
+++ src/app/(profile)/profile/page.jsx
@@ -2,18 +2,40 @@
 
 import { useGetUser } from "@/hooks/useAuth";
 import toLocalDateStringShort, { toLocalDateString } from "@/utils/toLocalDate";
+import Link from "next/link";
+import PaymentTable from "./payments/PaymentTable";
+import { FaEye } from "react-icons/fa";
 
 function Profile() {
   const { isLoading, data } = useGetUser();
-  const { user } = data || {};
+  const { user, payments } = data || {};
   if (isLoading) return <p>Loading....</p>;
   return (
     <div>
-      <h1>سلام! {user.name} خوش آمدید</h1>
+      <h1 className="mb-4">
+        سلام! <span className="font-bold">{user.name}</span> خوش آمدید
+      </h1>
       <p>
         <span>تاریخ پیوستن : </span>
         <span>{toLocalDateString(user.createdAt)}</span>
       </p>
+      <div className="border rounded-xl p-4 mt-8">
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold">آخرین سفارشات کاربر</h2>
+          <Link
+            className="flex items-center gap-x-1.5 border-b border-b-primary-900 text-primary-900"
+            href="/profile/payments"
+          >
+            <FaEye className="w-5 h-5" />
+            <span>همه سفارشات</span>
+          </Link>
+        </div>
+        <PaymentTable
+          payments={payments
+            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+            .slice(0, 3)}
+        />
+      </div>
     </div>
   );
 }
--- src/app/(profile)/profile/payments/page.jsx
+++ src/app/(profile)/profile/payments/page.jsx
@@ -1,10 +1,8 @@
 "use client";
 
 import Loading from "@/common/Loading";
-import { userPaymentTHeads } from "@/constants/tableHeads";
 import { useGetUser } from "@/hooks/useAuth";
-import { toLocalDateString } from "@/utils/toLocalDate";
-import { toPersianNumbersWithComma } from "@/utils/toPersianNumber";
+import PaymentTable from "./PaymentTable";
 
 function page() {
   const { isLoading, data } = useGetUser();
@@ -13,59 +11,9 @@ function page() {
   if (isLoading) return <Loading />;
 
   return (
-    <div>
-      <h1>سفارشات کاربر</h1>
-      <div className="shadow-sm overflow-auto my-8">
-        <table className="border-collapse table-auto w-full min-w-[800px] text-sm">
-          <thead>
-            <tr>
-              {userPaymentTHeads.map((item) => (
-                <th className="whitespace-nowrap table__th" key={item.id}>
-                  {item.label}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {payments.map((payment, index) => (
-              <tr key={payment._id}>
-                <td className="table__td">{index + 1}</td>
-                <td className="table__td whitespace-nowrap truncate">
-                  {payment.invoiceNumber}
-                </td>
-                <td className="table__td whitespace-nowrap max-w-[280px] truncate">
-                  {payment.description}
-                </td>
-                <td className="table__td">
-                  <div className="flex flex-col gap-y-2 items-start">
-                    {payment.cart.productDetail.map((product) => (
-                      <span
-                        className="badge badge--secondary"
-                        key={product._id}
-                      >
-                        {product.title}
-                      </span>
-                    ))}
-                  </div>
-                </td>
-                <td className="table__td font-bold">
-                  {toPersianNumbersWithComma(payment.amount)}
-                </td>
-                <td className="table__td whitespace-nowrap">
-                  {toLocalDateString(payment.createdAt)}
-                </td>
-                <td className="table__td">
-                  {payment.status === "COMPLETED" ? (
-                    <span className="badge badge--success">موفق</span>
-                  ) : (
-                    <span className="badge badge--error">ناموفق</span>
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <div>
+      <h1 className="font-bold text-lg">سفارشات کاربر</h1>
+      <PaymentTable payments={payments} />
     </div>
   );
 }
